a455d175b91f4da35d7d437e4ae86a36956f1e8a
Alexander Ramirez <alexram999rc@gmail.com>
2021-06-03 13:23:37 -0600

    Basic FXML and Window scene working properly

--- src/Controller/StarbuzzApplicationController.java
+++ src/Controller/StarbuzzApplicationController.java
@@ -1,4 +1,32 @@
 package Controller;
-//Commit
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class StarbuzzApplicationController {
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void onExit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void onLogin(ActionEvent event) {
+
+    }
 }
--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,7 +1,31 @@
 package Model;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class Main extends Application {
+
     public static void main(String[] args){
-        System.out.println("Hello World");
+        try{
+            launch(args);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("../View/StarbuzzApplication.fxml"));
+        AnchorPane window = loader.load();
+        Scene scene = new Scene(window);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
