26bc6ff5f4f26ab7da23ad5ea427b5a01abb8474
Alexander Ramirez <alexram999rc@gmail.com>
2021-06-05 18:26:38 -0600

    implemented main window controller and System Beverage method

--- src/Controller/StarbuzzApplicationControllerCoffee.java
+++ src/Controller/StarbuzzApplicationControllerCoffee.java
@@ -1,6 +1,44 @@
 package Controller;
 
-public class StarbuzzApplicationControllerCoffee {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
+public class StarbuzzApplicationControllerCoffee {
+    @FXML
+    private AnchorPane anPa;
+
+    @FXML
+    private Button espressoButton;
+
+    @FXML
+    private Button darkRoastButton;
+
+    @FXML
+    private Button decafButton;
+
+    @FXML
+    private Button houseBlendButton;
+
+    @FXML
+    void onDarkRoastSelection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDecafSelection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEspressoSelection(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHouseBlendSelection(ActionEvent event) {
+
+    }
 
 }
--- src/Model/SystemBeverage.java
+++ src/Model/SystemBeverage.java
@@ -1,15 +1,23 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class SystemBeverage {
-    public void generateBeverage(){
 
+
+    public String generateInvoice(ArrayList<Beverage> beverageList){
+        StringBuilder bill = new StringBuilder();
+        for(Beverage i: beverageList)
+        {
+            bill.append("-------------------------");
+           bill.append( i.getDescription());
+           bill.append('\n');
+           bill.append(i.getCost() + '\n');
+        }
+        return bill.toString();
     }
 
-    public String generateInvoice(){
-        return null;
-    }
-
-    public String printVoice(){
+    public String printInvoice(){
         return null;
     }
 
