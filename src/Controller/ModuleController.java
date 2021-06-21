297737f22caaf213867887d318f817e121feb548
Alexander Ramirez <alexram999rc@gmail.com>
2021-06-21 17:31:09 -0600

    added editing bill window capabilities

--- src/Controller/ModuleController.java
+++ src/Controller/ModuleController.java
@@ -2,53 +2,56 @@ package Controller;
 
 import Model.Beverage;
 import Model.SystemBeverage;
-import javafx.beans.property.ReadOnlyStringWrapper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+import javax.swing.*;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ModuleController implements Initializable {
 
     @FXML
-    private TableView<String> view;
+    private TextArea billOvervire;
 
     @FXML
-    private TableColumn<String, String> invoiceColumn;
+    private TextField beverageSelected;
 
     @FXML
-    private TableColumn<String, String> beverageColumn;
+    private Button deleteBeverageButton;
 
     @FXML
-    private TableColumn<String, String> toppingColumn;
-
-    @FXML
-    private TableColumn<String, String> priceColumn;
-
-    @FXML
-    private TableColumn<String, String> previousBillNumberColumn;
-
-    @FXML
-    private TableColumn<String, String> showPreviousBillColumn;
+    void onDeleteBeverage(ActionEvent event) {
+        try{
+            int selection = Integer.valueOf(beverageSelected.getText());
+            SystemBeverage.beverages.remove(selection - 1);
+            prepareBill();
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, "El valor ingresado no corresponde a una bebida en la factura");
+        }
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ArrayList<String> beverageName = new ArrayList<>();
-        for (Beverage x: SystemBeverage.beverages
-             ) {
-            beverageName.add(x.getBaseName());
-        }
-        beverageColumn.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
-        ObservableList<String> data = FXCollections.observableArrayList((List) (beverageName));
-        view.setItems(data);
+        prepareBill();
+    }
 
+    public void prepareBill()
+    {
+        StringBuilder currentBill = new StringBuilder();
+        int i = 1;
+        for (Beverage x: SystemBeverage.beverages
+        ) {
+            currentBill.append(String.valueOf(i) + ") ");
+            currentBill.append(x.getBaseName());
+            currentBill.append(x.getToppings());
+            currentBill.append(x.getCost());
+        }
+        billOvervire.setText(currentBill.toString());
     }
 }
